0d9ab75ec35e5d29caad46e03379cfc24397dec3
zhangWenFa <18831816834@163.com>
2022-09-21 10:19:33 +0800

    配置webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const a = 'Hello ITEM 12313';
+const a = 'Hello ITEM 12313 23434111';
 console.log(a);
 
 export {
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 const __dirname = path.resolve();
+console.log('process.env.NODE_ENV = ', process.env.NODE_ENV)
 
-export default {
-  mode: 'development',
+const config = {
   entry: './src/index.js',
   output: {
     filename: './js/bundle.js',
@@ -23,5 +23,19 @@ export default {
       template: './public/index.html'
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true, // 启动压缩gzip
+    port: 4005, // 端口号
+    open: true, // 是否自动打开浏览器
+  }
+}
+
+// env是运行的命令
+export default (env, args) => {
+  console.log('argv.mode = ', env, args.mode)
+  return config;
 }
